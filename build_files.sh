8c8f910e654398008d4f12e3e2f55ad085c43f6d
Nmait2111 <namitjain2111@gmail.com>
2023-04-16 00:00:55 +0530

    final updates multi

--- build_files.sh
+++
@@ -1,2 +0,0 @@
-echo "Start"
-python3.9 -m pip install -r requirement.txt
--- s8project/settings.py
+++ s8project/settings.py
@@ -117,12 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
-MEDIA_URL = '/images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
+# MEDIA_URL = '/images/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
